e4186ed69d36c44ad499f913160336af0f2aa81b
xx6MarcianoEstudador9xx <164871749+senhorN2@users.noreply.github.com>
2024-10-21 11:29:28 -0300

    fd

--- main.js
+++ main.js
@@ -6,3 +6,31 @@
     const campoPalpite = document.querySelector('.campoPalpite');
     let contagemPalpites = 1;
     let botaoReiniciar;
+
+    function verificarPalpite() {
+        const palpiteUsuario = Number(campoPalpite.value);
+        if (contagemPalpites === 1) {
+            palpites.textContent = "Palpites anteriores: ";
+        }
+
+        palpites.textContent += palpiteUsuario + " ";
+
+         if (palpiteUsuario === numeroAleatorio) {
+            ultimoResultado.textContent = "Parabéns! Você acertou";
+            ultimoResultado.style.backgroundColor = "green";
+            baixoOuAlto.textContent = "";
+            finalizarJogo();
+            } else if (contagemPalpites === 10) {
+                ultimoResultado.textContent = "FIM DE JOGO!!!";
+                baixoOuAlto.textContent = "";
+                finalizarJogo();
+            } else {
+                ultimoResultado.textContent = "Errado seu burro!!!!!!!";
+                ultimoResultado.style.backgroundColor = "red";
+                if (palpiteUsuario < numeroAleatorio) {
+                    baixoOuAlto.textContent = "muito baixo, burro!!!!!!"
+                } else if (palpiteUsuario > numeroAleatorio) {
+                    baixoOuAlto.textContent = "muito alto, burro!!!!!!"
+                }
+            }
+    }
